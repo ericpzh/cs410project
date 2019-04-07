af339affffd8195d4c481ccaeb58cc905a071375
Chen <799794275@qq.com>
2019-04-07 17:52:49 -0500

    parse data

--- frontend/app.py
+++ frontend/app.py
@@ -10,12 +10,15 @@ class InputForm(FlaskForm):
     def validate_field(self, field):
         if False: ##todo parse it
             raise ValidationError('Must enter more than 1 libraries.')
-            
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any secret string'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    with open("../scraper/data.txt") as file:
+        lib_list = [line[1:-2].split(",") for line in file.readlines()]
+
     form = InputForm()
     if form.validate_on_submit():
         list = [
@@ -33,6 +36,9 @@ def index():
         },
         {
             'name': '+1s',
+        },
+        {
+            'name': lib_list[0]
         }
     ]
     return render_template('index.html', title='APP', list=list, form=form)
@@ -40,4 +46,3 @@ def index():
 
 if __name__ == '__main__':
     app.run()
-
