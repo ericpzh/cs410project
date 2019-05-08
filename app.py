d3def2fee31095015229bfed05fc0da78c09dd0e
MeishanWu <meishan2@illinois.edu>
2019-05-07 23:24:40 -0500

    put searcher into the functions, update route name

--- app.py
+++ app.py
@@ -24,31 +24,21 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html', title='APP')
 
-@app.route('/test', methods=['GET'])
+@app.route('/api/package', methods=['GET'])
 def test():
     queryContent = request.args.get('data').replace(","," ")
     print("query is: ", queryContent)
     searcher = PackageSearcher("config.toml")
     results = searcher.search(queryContent)
     return jsonify(results)
-    # return jsonify(["n", "m", "s", "l"])
 
 @app.route('/api/description', methods=['GET'])
 def descriptionGet():
     queryContent = request.args.get('data')
     print("query is: ", queryContent)
-    results = app.searcher2.search(queryContent)
+    searcher =  DescriptionSearcher("config2.toml")
+    results = searcher.search(queryContent)
     return jsonify(results)
-    # return jsonify([{"title":"nm", "des":"sl", "key":["n", "m", "s", "l"]}])
-
-@app.route('/topic', methods=['GET'])
-def topic():
-    args = request.args.get('data').split(",")
-    print(args)
-    return jsonify(args)
 
 if __name__ == '__main__':
-    app.searcher = PackageSearcher("config.toml")
-    app.searcher2 = DescriptionSearcher("config2.toml")
-    # print(app.searcher2.search("framework"))
     app.run()
